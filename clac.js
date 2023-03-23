1e93f1a920d1671fec738f70bef4bf46b6df946d
SriSravya2002 <111737540+SriSravya2002@users.noreply.github.com>
2023-03-23 14:18:20 +0530

    Add files via upload

---
+++ clac.js
@@ -0,0 +1,9 @@
+var cl = require('./local')
+var x=20,y=10;
+console.log("Addition: "+cl.add(x,y));
+console.log("Subtraction: "+cl.sub(x,y));
+console.log("Multiplication: "+cl.mul(x,y));
+console.log("Division: "+cl.div(x,y));
+
+
+
